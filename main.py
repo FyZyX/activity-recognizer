5703b5b74a2fbbf5d0c9884393f1ea41e1d99f5a
Lucas Lofaro <lucasmlofaro@gmail.com>
2017-11-17 15:26:20 -0800

    added grid search to optimize classification

--- main.py
+++ main.py
@@ -9,8 +9,20 @@ if __name__ == '__main__':
     cluster = k_means(X_train, y_train)
     tree = decision_tree(X_train, y_train)
 
+    cluster_gs = GridSearchCV(estimator=cluster, param_grid={'n_init': range(1, 11)}, refit=True)
+    cluster_gs.fit(X_train, y_train)
+    print(cluster_gs.best_params_)
+
+    tree_gs = GridSearchCV(estimator=tree, param_grid={
+        'max_depth': range(1, 11),
+        'min_samples_split': range(5, 55),
+        'min_samples_leaf': range(2, 11)
+    }, refit=True)
+    tree_gs.fit(X_train, y_train)
+    print(tree_gs.best_params_)
+
     # visualize tree
     generate_tree(tree)
 
-    print(accuracy(cluster, X_test, y_test))
-    print(accuracy(tree, X_test, y_test))
+    print(accuracy(cluster_gs, X_test, y_test))
+    print(accuracy(tree_gs, X_test, y_test))
